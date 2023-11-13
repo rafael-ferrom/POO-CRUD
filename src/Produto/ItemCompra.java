2b4d021fc5931f85439c6970903a2b030af8bbd5
Rafael Ferro <rafaelferromac@gmail.com>
2023-11-13 19:29:51 -0300

    Clients

---
+++ src/Produto/ItemCompra.java
@@ -0,0 +1,37 @@
+package Produto;
+
+import java.io.Serializable;
+
+public class ItemCompra implements Serializable{
+    
+    private int qntde;
+	private Produto produto;
+	
+	public itemCompra() {
+		
+	}
+
+	public itemCompra(int qntde, Produto produto) {
+		super();
+		this.qntde = qntde;
+		this.produto = produto;
+	}
+
+	public int getQntde() {
+		return qntde;
+	}
+
+	public void setQntde(int qntde) {
+		this.qntde = qntde;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+
+}
---
+++ src/arquivoSistema.java
@@ -0,0 +1,77 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class arquivoSistema {
+
+	public static boolean gravarObjeto(Object obj, String caminho) {
+
+		File arquivo = new File(caminho);
+
+		if (!arquivo.exists()) {
+			try {
+				arquivo.createNewFile();
+			} // try
+			catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			} // catch
+		} // if
+
+		try {
+			FileOutputStream fileOutput = new FileOutputStream(arquivo);
+			ObjectOutputStream objOutput = new ObjectOutputStream(fileOutput);
+
+			objOutput.writeObject(obj);
+
+			objOutput.flush();
+			fileOutput.flush();
+
+			objOutput.close();
+			fileOutput.close();
+
+			return true;
+		} // try
+		catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} // catch
+
+	}// gravarObjeto
+
+	public static Object recuperarObjeto(Object obj, String caminho) {
+
+		File arquivo = new File(caminho);
+
+		if (!arquivo.exists()) {
+			try {
+				arquivoSistema.gravarObjeto(obj, caminho);
+			} // try
+			catch (Exception e) {
+				e.printStackTrace();
+			} // catch
+			return obj;
+		} // if
+		else {
+			try {
+				FileInputStream fileInput = new FileInputStream(arquivo);
+				ObjectInputStream objInput = new ObjectInputStream(fileInput);
+
+				Object retorno = objInput.readObject();
+
+				objInput.close();
+				fileInput.close();
+
+				return retorno;
+
+			} // try
+			catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			} // catch
+		} // else
+	}// recuperarObjeto
+
+}// arquivoSistema
